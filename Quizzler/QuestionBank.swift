d2b64f0999f3f4f4ceda82b1ef233cfba7605ca4
Charity <Youngblood.charity@gmail.com>
2018-01-21 13:41:42 -0800

    created QuestionBank class

--- Quizzler/QuestionBank.swift
+++ Quizzler/QuestionBank.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+
+// the QuestionBank class will store all questions that we will use in the quiz app
+// each question will have a questionText and answer property
+class QuestionBank {
+    // create an array of question objects
+    var list = [] // this variable is a property since it is within the class QuestionBank
+    // we can either give the array actual values or the type of data that will be passed through (i.e. Int or String)
+}
