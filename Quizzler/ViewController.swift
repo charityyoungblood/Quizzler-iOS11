fd7916d4bf5bef4c208651c4b0b2dec42d8f3ab7
charityyoungblood <youngblood.charity@gmail.com>
2018-01-22 10:25:30 -0800

    created methods for checkedAnswer and answerPressed

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     let allQuestions = QuestionBank() // instead of Question.new to initialize a new Question instance - use the class name and () - if there are parameters, you would add them to the parentheses, in this case QuestionBank has no parameters so we leave () blank 
     
+    let pickedAnswer : Bool = false //initial value of false  - this can be re-assigned to another value later
+    
     //Place your instance variables here
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -24,12 +26,23 @@ class ViewController: UIViewController {
         
         let firstQuestion = allQuestions.list[0]
         questionLabel.text = firstQuestion.questionText
-        
     }
 
-
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        // in order for us to check if the user got the question right or wrong, we have to compare their selected answer versus the correct answer
+        // answer == user input or user answer
+        // something.tag
+        // did user select tag 1 (true) or tag 2 (false)
+        
+        if sender.tag == 1 {
+            // we will set pickedAnswer to true
+            pickedAnswer = true
+        }
+        
+        else if sender.tag == 2 {
+            pickedAnswer = false 
+        }
+        checkAnswer()
     }
     
     
@@ -44,7 +57,15 @@ class ViewController: UIViewController {
     
     
     func checkAnswer() {
-        
+        // iS Question.answer or question.correctAnswer == "true"
+        // answerPressed == question.answer
+        let correctAnswer = allQuestions.list[0].answer
+        if correctAnswer == pickedAnswer {
+            print("You got it!")
+        }
+        else {
+            
+        }
     }
     
     
